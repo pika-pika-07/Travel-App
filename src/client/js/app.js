2a29a7229c223208ae93e099fd75b2529171dfa2
parth bhatnagar <parthbhatnagar@192.168.1.3>
2023-06-30 00:46:09 +0530

    Added date check

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,91 +1,89 @@
 /* Global Variables */
-import {getDays} from './helper'
+import { getDays, checkUserInput } from "./helper";
 
-const apiBaseURL = 'http://localhost:3000'
-
+const apiBaseURL = "http://localhost:3000";
 
 export const plan = async () => {
-    const destinationCity = document.querySelector('#input-destination').value
-    const startDate = document.querySelector("#input-start-date").value
-    const endDate = document.querySelector('#input-end-date').value
+  const destinationCity = document.querySelector("#input-destination").value;
+  const startDate = document.querySelector("#input-start-date").value;
+  const endDate = document.querySelector("#input-end-date").value;
 
-   
-    const diffDays = getDays(startDate,endDate)
+  try {
+    checkUserInput(destinationCity, startDate, endDate);
 
-    const geoData  = await fetchGeoCordinates(destinationCity)
-    const coordinates = geoData.geonames[0]
+    const diffDays = getDays(startDate, endDate);
 
-    const forecastData = await fetchForecast(diffDays,coordinates.lat,coordinates.lng)
+    const geoData = await fetchGeoCordinates(destinationCity);
+    const coordinates = geoData.geonames[0];
 
-    const imageUrl = await fetchImage(destinationCity)
-    
+    const forecastData = await fetchForecast(
+      diffDays,
+      coordinates.lat,
+      coordinates.lng
+    );
+
+    const imageUrl = await fetchImage(destinationCity);
+
     const obj = {
-        city:destinationCity,
-        imageUrl: imageUrl,
-        diffDays: diffDays,
-        temp: forecastData.temperature,
-        description: forecastData.description,
-        countryName: coordinates.countryName
-    }
-    await updateUI(obj)
-    
-}
-
+      city: destinationCity,
+      imageUrl: imageUrl,
+      diffDays: diffDays,
+      temp: forecastData.temperature,
+      description: forecastData.description,
+      countryName: coordinates.countryName,
+    };
+    await updateUI(obj);
+  } catch (err) {
+    console.log(err);
+    return;
+  }
+};
 
 const fetchGeoCordinates = async (location) => {
+  const url = `${apiBaseURL}/geoNames`;
+  const response = await fetch(`${url}?destination=${location}`);
 
-    const url = `${apiBaseURL}/geoNames`;
-    const response = await fetch(`${url}?destination=${location}`)
-   
-    try {
-        const data = await response.json()
-        console.log("resSSSSSS", data)
-        return data
-    }
-    catch(err){
-        console.log("Error",err)
-    }
-}
+  try {
+    const data = await response.json();
+    console.log("resSSSSSS", data);
+    return data;
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
 
 const fetchForecast = async (days, lat, lng) => {
-    const url = `${apiBaseURL}/forecast`;
-  
-    const response = await fetch(`${url}?days=${days}&lat=${lat}&lon=${lng}`)
-   
-    try {
-        const data = await response.json()
-       
-        return data
-    }
-    catch(err){
-        console.log("Error",err)
-    }
+  const url = `${apiBaseURL}/forecast`;
 
-}
+  const response = await fetch(`${url}?days=${days}&lat=${lat}&lon=${lng}`);
+
+  try {
+    const data = await response.json();
+
+    return data;
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
 
 const fetchImage = async (city) => {
-    const apiKey = '37685063-6f00dae4135d76e834728903c'
-    const url = `https://pixabay.com/api/?key=${apiKey}&category=places&q=${city}&image_type=photo&pretty=true`
+  const apiKey = "37685063-6f00dae4135d76e834728903c";
+  const url = `https://pixabay.com/api/?key=${apiKey}&category=places&q=${city}&image_type=photo&pretty=true`;
 
-    const response = await fetch(url)
-   
-    try {
-        const data = await response.json()
-        const img = data['hits'][0]["webformatURL"]
-        return img
-    }
-    catch(err){
-        console.log("Error",err)
-    }
+  const response = await fetch(url);
 
-}
+  try {
+    const data = await response.json();
+    const img = data["hits"][0]["webformatURL"];
+    return img;
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
 
 const updateUI = async (data) => {
-    document.getElementById('img').setAttribute('src', data.imageUrl);
-    document.getElementById('result-div').innerHTML = `<p>${data.city} is ${data.diffDays} days away.</p><p>The weather for that time is: <br> Temperature: ${data.temp} <br> ${data.description}</p>`
-}
-
-
-
-
-
+  document.getElementById("img").setAttribute("src", data.imageUrl);
+  document.getElementById(
+    "result-div"
+  ).innerHTML = `<p>${data.city} is ${data.diffDays} days away.</p><p>The weather for that time is: <br> Temperature: ${data.temp} <br> ${data.description}</p>`;
+};
--- src/client/js/helper.js
+++ src/client/js/helper.js
@@ -1,10 +1,51 @@
-export const getDays = (startDate,endDate) => {
-    const date1 = new Date(startDate)
-    const date2 = new Date(endDate)
-    
-    const diffTime = Math.abs(date2 - date1);
-    const diff = Math.ceil(diffTime / (1000 * 60 * 60 * 24))
-    const diffDays =  diff <= 16 ? diff -1 : 15; 
+export const getDays = (startDate, endDate) => {
+  const date1 = new Date(startDate);
+  const date2 = new Date(endDate);
 
-    return diffDays
-}
+  const diffTime = Math.abs(date2 - date1);
+  const diff = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+  const diffDays = diff <= 16 ? diff - 1 : 15;
+
+  return diffDays;
+};
+
+export const checkUserInput = (destination, startDate, endDate) => {
+  try {
+    console.log("1.1) Start function checkUserInput");
+
+    // check that 'destination' is not empty
+    if (!destination) {
+      alert("Please enter a destination.");
+      throw new Error("Please enter a destination.");
+    }
+
+    // check that 'start-date' is not empty
+    if (!startDate) {
+      alert("Please enter a departing date.");
+      throw new Error("Please enter a departing date.");
+    }
+
+    // create date objects for comparison
+    const currentDate = new Date();
+    const departingDate = new Date(startDate);
+    const returningDate = new Date(endDate);
+
+    // check that 'start-date' is not in the past
+    if (departingDate < currentDate) {
+      alert("The departing date must be in the future!");
+      throw new Error("The departing date must be in the future!");
+    }
+
+    // check that 'end-date' is greater than 'start-date', if it wasn't left empty
+    if (endDate !== "" && returningDate < departingDate) {
+      alert("The returning date must be greater than the departing date!");
+      throw new Error(
+        "The returning date must be greater than the departing date!"
+      );
+    }
+  } catch (error) {
+    // Error handling
+    console.log("Error function checkUserInput: ", error);
+    throw error;
+  }
+};
